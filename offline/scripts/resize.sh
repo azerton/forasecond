441d291204aacb6959b0730cd4d286ff5e633b49
Daan Raman <draman@nviso.be>
2015-11-17 23:20:58 +0100

    scripts accept spaces in filenames

--- offline/scripts/resize.sh
+++ offline/scripts/resize.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
-DIRNAME=`dirname $1`
-OUTPUTDIR=`dirname $1`/trimmed/
-FILENAME=`basename $1`
+DIRNAME=`dirname "$1"`
+OUTPUTDIR=`dirname "$1"`/trimmed/
+FILENAME=`basename "$1"`
 
-ffmpeg -y -i $DIRNAME/trimmed/$FILENAME.ts -vf scale=1280:720 -vcodec libx264 -acodec copy $DIRNAME/trimmed/${FILENAME}_resized.ts;
+ffmpeg -y -i "$DIRNAME/trimmed/$FILENAME.ts" -vf scale=1280:720 -vcodec libx264 -acodec copy "$DIRNAME/trimmed/${FILENAME}_resized.ts";
--- offline/scripts/timestamp.sh
+++ offline/scripts/timestamp.sh
@@ -1,12 +1,12 @@
 #!/bin/bash
-DIRNAME=`dirname $1`
-OUTPUTDIR=`dirname $1`/trimmed/
-FILENAME=`basename $1`
+DIRNAME=`dirname "$1"`
+OUTPUTDIR=`dirname "$1"`/trimmed/
+FILENAME=`basename "$1"`
 
-ffmpeg -y -i $DIRNAME/trimmed/$FILENAME.ts -vf drawtext="fontfile=/Users/draman/Library/Fonts/Swiss-721-WGL4-BT_43266.ttf: \
+ffmpeg -y -i "$DIRNAME/trimmed/$FILENAME.ts" -vf drawtext="fontfile=/Users/draman/Library/Fonts/Swiss-721-WGL4-BT_43266.ttf: \
 		fontcolor=white@0.8: fontsize=24: \
 		expansion=strftime:basetime=$(date +%s)000000: \
 	    text='$newline %b %d, %Y $newline': \
-		x=(w-text_w)-30: y=20" -vcodec libx264 -acodec libvo_aacenc $DIRNAME/trimmed/${FILENAME}_txt.ts;		
+		x=(w-text_w)-30: y=20" -vcodec libx264 -acodec libvo_aacenc "$DIRNAME/trimmed/${FILENAME}_txt.ts";		
 		
 		
