0e83920d00c07d07501e09d79e7ea2761f1916d3
Daan Raman <draman@nviso.be>
2016-03-26 01:08:27 +0100

    allow more than 256 files to be concatenated

--- offline/forasecond.sh
+++ offline/forasecond.sh
@@ -8,7 +8,7 @@ rm $RAWPATH/trimmed/*.ts
 OIFS="$IFS"
 IFS=$'\n'
 
-for f in `ls -t $RAWPATH/*.{mov,MOV}`;
+for f in `ls -t $RAWPATH/*.{mov,MOV,mp4,MP4}`;
 do
 	echo "Processing $f"
 	echo "Converting and trimming file format"
@@ -20,4 +20,14 @@ do
 done
 
 #Now, multiplex all short clips together - overwrite the output file without asking for confirmation.
-ffmpeg -y -i "concat:$(ls -1 $RAWPATH/trimmed/*_resized.ts | perl -0pe 's/\n/|/g;s/\|$//g')" -async 1 -vcodec libx264 -bsf:a aac_adtstoasc $RAWPATH/../forasecond.mp4
+> input.txt
+
+for f in `ls -1 $RAWPATH/trimmed/*_resized.ts`;
+do
+	echo "Concatenating $f"
+	echo "file $f" >> input.txt
+done
+
+#ffmpeg -y -i "concat:$(ls -1 $RAWPATH/trimmed/*_resized.ts | perl -0pe 's/\n/|/g;s/\|$//g')" -async 1 -vcodec libx264 -bsf:a aac_adtstoasc $RAWPATH/../forasecond.mp4
+ffmpeg -y -f concat -analyzeduration 100M -probesize 100M -i input.txt -async 1 -vcodec libx264 -bsf:a aac_adtstoasc $RAWPATH/../forasecond.mp4
+#rm input.txt
--- offline/scripts/convert_format.sh
+++ offline/scripts/convert_format.sh
@@ -2,4 +2,5 @@
 OUTPUTDIR=`dirname "$1"`/trimmed/
 FILENAME=`basename "$1"`
 echo "Outputting converted file to $OUTPUTDIR"
-ffmpeg -y -i "${1}" -ss 00:00:00 -t 00:00:01 -vf scale=1280:720 -vcodec libx264 -acodec copy "$OUTPUTDIR/${FILENAME}_resized.ts";
+FILENAME_UNDERSCORED=`echo ${FILENAME// /_}`
+ffmpeg -y -i "${1}" -ss 00:00:00 -t 00:00:01 -vf scale=1280:720 -vcodec libx264 -acodec copy "$OUTPUTDIR/${FILENAME_UNDERSCORED}_resized.ts";
