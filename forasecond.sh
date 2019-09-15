2e085fa2f7585f4a7afd7e804a8522f283d4d333
Daan Raman <draman@nviso.be>
2019-09-15 16:27:21 +0200

    updates

--- forasecond.sh
+++ forasecond.sh
@@ -1,37 +1,37 @@
 #!/bin/bash
 PROJDIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
 
-# Move to the project parent folder
-cd $PROJDIR
-
-MOVPATH=$1
-
-#Convert and trim all raw clips
-mkdir $PROJDIR/tmp/trimmed
-rm $PROJDIR/tmp/trimmed/*.ts
-
 #Handle spaces in filenames
 OIFS="$IFS"
 IFS=$'\n'
 
-for f in `find "$MOVPATH" -type f -name "*.mov" -o -name "*.MOV" -o -name "*.mp4" -o -name "*.MP4"`;
+# Move to the project parent folder
+cd "$PROJDIR"
+MOVPATH=$1
+
+# Convert and trim all raw clips
+mkdir "$PROJDIR/tmp/trimmed"
+rm "$PROJDIR/tmp/trimmed/*.ts"
+
+# Empty the file list
+> $PROJDIR/tmp/input.txt
+
+for f in `find "$MOVPATH" -type f -name "*.mov" -o -name "*.MOV" -o -name "*.mp4" -o -name "*.MP4"|xargs stat -f '%c %N'|sort|cut -d ' ' -f 2-`;
 do
 	echo "Processing $f"
 	echo "Converting and trimming file format"
-	./scripts/convert_format.sh "$f"
-	#echo "Resizing clip"
-	#./scripts/resize.sh "$f"
+
+  OUTPUTDIR="$PROJDIR/tmp/trimmed"
+  FILENAME=`basename "$f"`
+  echo "Outputting converted file to $OUTPUTDIR"
+  FILENAME_UNDERSCORED=`echo ${FILENAME// /_}`
+  ffmpeg -y -i "$f" -ss 00:00:00 -t 00:00:01 -vf "scale=min(iw*720/ih\,1280):min(720\,ih*1280/iw),pad=1280:720:(1280-iw)/2:(720-ih)/2" -vcodec libx264 -acodec copy "$OUTPUTDIR/${FILENAME_UNDERSCORED}.ts";
+
+  echo "Including $f in list of files to concatenate"
+
+  echo "file $OUTPUTDIR/${FILENAME_UNDERSCORED}.ts" >> tmp/input.txt
 	echo ""
 #	./scripts/timestamp.sh $f
-done
-
-#Now, multiplex all short clips together - overwrite the output file without asking for confirmation.
-> $PROJDIR/tmp/input.txt
-
-for f in `ls -1 $PROJDIR/tmp/trimmed/*.ts`;
-do
-	echo "Including $f in list of files to concatenate"
-	echo "file $f" >> tmp/input.txt
 done
 
 ffmpeg -y -f concat -analyzeduration 100M -probesize 100M -safe 0 -i tmp/input.txt -async 1 -vcodec libx264 -bsf:a aac_adtstoasc $PROJDIR/forasecond.mp4
